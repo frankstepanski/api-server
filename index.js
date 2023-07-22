488bf1311ab747e264f01bdc9b31eb3d46030335
Frank Stepanski <fsjay@hotmail.com>
2023-07-22 17:32:49 -0400

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const jsonServer = require("json-server");
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    })
+);
+
+server.options('*', cors());
+
 server.use(middlewares);
 server.use(router);
-
 server.listen(port, () => {
     console.log("JSON Server is running");
 });
